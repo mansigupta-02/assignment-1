1abdafae9a1efe192ddb2b50f78b11b46e6c9d86
mansigupta-02 <mansigupta2122006@gmail.com>
2026-07-21 21:12:34 +0530

    Add files via upload

---
+++ Day 1/area.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int l,b;
+    printf("enter the length \n");
+    scanf("%d",&l);
+    printf("enter the breadth");
+    scanf("%d",&b);
+    printf("the area of rectangle of length %d and breadth %d is %d", l,b,l*b);
+    return 0;
+}
